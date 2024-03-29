e147c7b10c89d2838b7411752c890cae16ad5699
SashaLevkovich <aleksandr.levkovicha@gmail.com>
2024-03-29 19:32:45 +0300

    [sprint_2] Refactor file name

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { Login } from './login/LoginPage'
 export { SignUp } from './signUp/SignUpPage'
-export { Chats } from './chats/ChatsPage'
+export { Chats } from './chats/chats'
 export { UserProfile } from './userProfile/UserProfilePage'
 export { ChangePassword } from './changePassword/ChangePasswordPage'
 export { Error } from './Error/ErrorPage'
