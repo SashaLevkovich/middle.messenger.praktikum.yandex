e3cd395f9d18caa8563c482fea27c156d76759ab
SashaLevkovich <aleksandr.levkovicha@gmail.com>
2024-05-05 21:37:31 +0300

    [sprint_4] Add new test for block

--- test/block.test.ts
+++ test/block.test.ts
@@ -6,6 +6,14 @@ class TestBlock extends Block {
     super({ testText })
   }
 
+  getTestElement() {
+    return this.getElement
+  }
+
+  setTestElement(value: HTMLElement) {
+    this.setElement = value
+  }
+
   render() {
     return `<div>{{ testText }}</div>`
   }
@@ -17,5 +25,26 @@ describe('Block', () => {
 
   it('should render new component', () => {
     expect(block).to.equal('Test text')
+  })
+
+  it('should add attributes to the element', () => {
+    component.setTestElement(document.createElement('div'))
+    component.setProps({ attr: { 'data-test': '123' } })
+    component.addAttributes()
+    expect(component.getTestElement()?.getAttribute('data-test')).to.equal(
+      '123',
+    )
+  })
+
+  it('should show and hide the element', () => {
+    component.setTestElement(document.createElement('div'))
+    component.show()
+    expect(
+      document.querySelector('#root')?.contains(component.getTestElement()!),
+    ).to.be.true
+    component.hide()
+    expect(
+      document.querySelector('#root')?.contains(component.getTestElement()!),
+    ).to.be.false
   })
 })
